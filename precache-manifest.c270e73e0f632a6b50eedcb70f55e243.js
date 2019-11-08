f8ae6aec4e34eb6f0731e5fff971f43b802aa187
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 14:16:26 -0300

    Updates

--- precache-manifest.c270e73e0f632a6b50eedcb70f55e243.js
+++ precache-manifest.c270e73e0f632a6b50eedcb70f55e243.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee1b2fafa1ec1c42e166e5687052aa94",
+    "revision": "979c8fa4d7946ad0050a69fea3cff7cc",
     "url": "/FirstBlogs/index.html"
   },
   {
-    "revision": "7cf59af89787c9e50972",
+    "revision": "6c01cefef3a1bec669d2",
     "url": "/FirstBlogs/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FirstBlogs/static/js/2.044c3c48.chunk.js"
   },
   {
-    "revision": "7cf59af89787c9e50972",
-    "url": "/FirstBlogs/static/js/main.76a9a4a8.chunk.js"
+    "revision": "6c01cefef3a1bec669d2",
+    "url": "/FirstBlogs/static/js/main.e00ab2cb.chunk.js"
   },
   {
     "revision": "2a23da4dfbd922816cbe",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FirstBlogs/precache-manifest.db480fe2e6654298464ad39ea07a8cf8.js"
+  "/FirstBlogs/precache-manifest.c270e73e0f632a6b50eedcb70f55e243.js"
 );
 
 self.addEventListener('message', (event) => {
