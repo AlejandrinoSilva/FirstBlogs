76f22f6b7e08f93c81ffadbb7b8dd8740d4806db
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 10:07:37 -0300

    Deleting

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, HashRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Contact } from './components/Contact';
-import { NoMatch } from './components/NoMatch';
-import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavigationBar';
-import { Jumbotron } from './components/Jumbotron';
-import { FooterBar } from './components/FooterBar';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter basename='/'>
-        <Fragment>
-          <NavigationBar />
-          <Jumbotron />
-          <Layout>
-            <Router>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-                <Route component={NoMatch} />
-              </Switch>
-            </Router>
-          </Layout>
-          <FooterBar />
-        </Fragment>
-      </HashRouter>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
