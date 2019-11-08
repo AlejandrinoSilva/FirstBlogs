570c6d978c38f1f0a2a698d6e7fa35c7e2ef8587
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 09:36:51 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AlejandrinoSilva/FirstWebPage/precache-manifest.358f8ee1bb8bef9964703427984e1971.js"
+  "/AlejandrinoSilva/FirstWebPage/precache-manifest.501ae96c83f686f79ee61edaa8433ff4.js"
 );
 
 self.addEventListener('message', (event) => {
