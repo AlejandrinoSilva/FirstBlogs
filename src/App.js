68fc9667a6362bcd1315a08335fa155a72ccad2f
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 10:41:03 -0300

    Mejoras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Layout } from './Layout';
+import { NavigationBar } from './NavigationBar';
+import { Home } from './Home';
+import { FooterBar } from './FooterBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>Edit <code>src/App.js</code> and save to reload.</p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </Switch>
+          </Router>
+        </Layout>
+        <FooterBar />
+      </Fragment>
+
+    );
+  }
+
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    .div-all{
+        position: relative;
+    }
+    
+`;
+
+export const Home = () => (
+    <Styles>
+        <h2>What is Lorem Ipsum? / Home</h2>
+        <div className="row mb-2">
+            <div className="col-md-6">
+                <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div className="col p-4 d-flex flex-column position-static">
+                        <strong className="d-inline-block mb-2 text-primary">World</strong>
+                        <h3 className="mb-0">Featured post</h3>
+                        <div className="mb-1 text-muted">Nov 12</div>
+                        <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="/" className="stretched-link">Continue reading</a>
+                    </div>
+                    <div className="col-auto d-none d-lg-block">
+                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div className="col p-4 d-flex flex-column position-static">
+                        <strong className="d-inline-block mb-2 text-success">Design</strong>
+                        <h3 className="mb-0">Post title</h3>
+                        <div className="mb-1 text-muted">Nov 11</div>
+                        <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <a href="/" className="stretched-link">Continue reading</a>
+                    </div>
+                    <div className="col-auto d-none d-lg-block">
+                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </Styles>
+)
