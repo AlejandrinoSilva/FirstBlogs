8bbb1f1bdcab5896910c138d5403067428447dd0
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 13:21:19 -0300

    degrade

---
+++ build/precache-manifest.8af01efd7c5a84826827333b9737ca9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabc25a31d24fe2af1f010c0db378ff8",
+    "url": "/FirstBlogs/index.html"
+  },
+  {
+    "revision": "3d351b84295be115ab60",
+    "url": "/FirstBlogs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f794ca9cc36b28c93e68",
+    "url": "/FirstBlogs/static/js/2.044c3c48.chunk.js"
+  },
+  {
+    "revision": "3d351b84295be115ab60",
+    "url": "/FirstBlogs/static/js/main.f7a78938.chunk.js"
+  },
+  {
+    "revision": "2a23da4dfbd922816cbe",
+    "url": "/FirstBlogs/static/js/runtime-main.453e9ec5.js"
+  }
+]);
