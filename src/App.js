65175585a9dc31fed70bc539ec4c1be5155f9992
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 10:53:53 -0300

    Mas Mod

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import { Layout } from './Layout';
-import { NavigationBar } from './NavigationBar';
-import { Home } from './Home';
-import { FooterBar } from './FooterBar';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StylesNav = styled.div`
+    .navbar{
+        background-color: black;
+        position: relative;
+    }
+
+    .navbar-brand, .navbar-nav .nav-link {
+        color: white;
+        &:hover {
+            color:white;
+        }
+    }
+
+`;
+const StylesFoo = styled.footer`
+    .con-color {
+        background-color: black;
+        position:relative; 
+        bottom: 0; 
+        width: 100%;
+    }
+    
+`;
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavigationBar />
+        <StylesNav>
+          <Navbar expand="lg fixed-top">
+            <Navbar.Brand href="/">Poemas del Alma</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </StylesNav>
+
         <Layout>
           <Router>
             <Switch>
@@ -33,7 +68,14 @@ class App extends Component {
             </Switch>
           </Router>
         </Layout>
-        <FooterBar />
+
+        <StylesFoo>
+          <footer className="footer mt-auto py-3 con-color">
+            <div className="container">
+              <span className="text-muted">Pagina con licencia CC</span>
+            </div>
+          </footer>
+        </StylesFoo>
       </Fragment>
 
     );
