3f85a5bbe725e6b462fa618a0f5d525561136070
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 16:35:36 -0300

    New

---
+++ build/precache-manifest.5f9d5966c4643ecde617c0d8da3744ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e12618f47a974833ce04c035ee15df",
+    "url": "/FirstBlogs/index.html"
+  },
+  {
+    "revision": "cc8fca8e3f89c633e7a4",
+    "url": "/FirstBlogs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f794ca9cc36b28c93e68",
+    "url": "/FirstBlogs/static/js/2.044c3c48.chunk.js"
+  },
+  {
+    "revision": "cc8fca8e3f89c633e7a4",
+    "url": "/FirstBlogs/static/js/main.b52e3d62.chunk.js"
+  },
+  {
+    "revision": "2a23da4dfbd922816cbe",
+    "url": "/FirstBlogs/static/js/runtime-main.453e9ec5.js"
+  }
+]);
