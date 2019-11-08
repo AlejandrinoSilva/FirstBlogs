b15626112cf04180e0a4a52be03e8ab2d05195f7
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-08 11:09:32 -0300

    Change

---
+++ build/precache-manifest.0abcdac84becfb464f874c38eb06f0cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5537ce3ef64ccb9fcded559eae06cbd0",
+    "url": "/FirstWebPage/index.html"
+  },
+  {
+    "revision": "69363a168b1f855699c9",
+    "url": "/FirstWebPage/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3aba78cbc70c502c587c",
+    "url": "/FirstWebPage/static/js/2.5783db1a.chunk.js"
+  },
+  {
+    "revision": "69363a168b1f855699c9",
+    "url": "/FirstWebPage/static/js/main.3bccdea9.chunk.js"
+  },
+  {
+    "revision": "9e69e8c1a406b6cf7680",
+    "url": "/FirstWebPage/static/js/runtime-main.95e42028.js"
+  }
+]);
