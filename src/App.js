5395abb292142de41513689bfa5697ae013bfb0c
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-12 20:48:13 -0300

    Quit NoMatch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
-import { NoMatch } from './components/NoMatch';
 import { Layout } from './components/Layout';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -94,7 +93,6 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
-            <Route component={NoMatch} />
           </Router>
         </Layout>
 
