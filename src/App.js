730ed9f4494a39ee9720ed9f61e498d35c7bcfd6
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-12 22:00:59 -0300

    Delete

--- src/App.js
+++ src/App.js
@@ -89,8 +89,8 @@ class App extends Component {
 
         <Container>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Route path="#/about" component={About} />
+          <Route path="#/contact" component={Contact} />
           <Route component={NoMatch} />
         </Container>
 
