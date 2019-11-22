08830a3a6bf58330428fcbd8551c231fbc0bc11d
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2019-11-22 13:56:29 -0300

    PostData

--- src/components/Home.js
+++ src/components/Home.js
@@ -54,33 +54,17 @@ export const Contact = () => (
 export const PostList = () => (
     PostData.map((postDetail, index) => {
         return (
-            <div className="row mb-2">
-                <div className="col-md-6">
-                    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div className="col p-4 d-flex flex-column position-static">
-                            <strong className="d-inline-block mb-2 text-primary">World</strong>
-                            <h3 className="mb-0">{postDetail.title}</h3>
-                            <div className="mb-1 text-muted">{postDetail.date}</div>
-                            <p className="card-text mb-auto">{postDetail.content}</p>
-                            <a href="/" className="stretched-link">Continue reading 1</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div className="col p-4 d-flex flex-column position-static">
-                            <strong className="d-inline-block mb-2 text-success">Design</strong>
-                            <h3 className="mb-0">Post title</h3>
-                            <div className="mb-1 text-muted">Nov 11</div>
-                            <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                            <a href="/" className="stretched-link">Continue reading 2</a>
-                        </div>
-                        <div className="col-auto d-none d-lg-block">
-                            <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                        </div>
+            <div className="row">
+                <div className="col-md-8 blog-main">
+                    <h3 className="pb-4 mb-4 font-italic border-bottom">From the Firehose</h3>
+
+                    <div className="blog-post">
+                        <h2 className="blog-post-title">{postDetail.title}</h2>
+                        <p className="blog-post-meta">{postDetail.date} by <a href="#">Mark</a></p>
+                        <p>{postDetail.content}</p>
                     </div>
                 </div>
             </div>
         )
-    })
-)
+    }
+    )
