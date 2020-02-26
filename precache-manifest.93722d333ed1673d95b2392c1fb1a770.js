61c4f61964d369ad1aefe7fb520371600f43f123
AlejandrinoSilva <ramon_al_sil@yahoo.com.ar>
2020-02-26 04:54:06 -0300

    Updates

---
+++ precache-manifest.93722d333ed1673d95b2392c1fb1a770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae411199b33d3d0e77fcd1f057ace26",
+    "url": "/FirstBlogs/index.html"
+  },
+  {
+    "revision": "217ac60c8a758fe5a953",
+    "url": "/FirstBlogs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8b46b8d7b7bac70757a2",
+    "url": "/FirstBlogs/static/js/2.0dbf8095.chunk.js"
+  },
+  {
+    "revision": "217ac60c8a758fe5a953",
+    "url": "/FirstBlogs/static/js/main.1523f7ea.chunk.js"
+  },
+  {
+    "revision": "2a23da4dfbd922816cbe",
+    "url": "/FirstBlogs/static/js/runtime-main.453e9ec5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FirstBlogs/precache-manifest.dfa0648717ec32468cc0c255cbd80f89.js"
+  "/FirstBlogs/precache-manifest.93722d333ed1673d95b2392c1fb1a770.js"
 );
 
 self.addEventListener('message', (event) => {
